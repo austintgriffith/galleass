2f11096983f32ea1540ef17f626495ac8c68406a
Austin <austin.griffith@madwiremedia.com>
2018-02-08 05:49:22 -0700

    fixed the experience test bug

--- tests/buyAndSellShips.js
+++ tests/buyAndSellShips.js
@@ -7,12 +7,19 @@ galleass.attemptBuyShipCheap(2,"Dogger")// this went through, you should make su
 //shouldn't be able to see ship if you don't have one
 galleass.attemptToSellFirstShip(2,"Dogger")
 
+//check the the experience works
+galleass.shouldNotHaveExperience(2,1)
 galleass.buyShip(2,"Dogger")
+galleass.shouldHaveExperience(2,1)
 
 //shouldn't be able to sell ship if you haven't approved
 galleass.attemptToSellFirstShip(2,"Dogger")
 
+
+
 galleass.buyShip(2,"Dogger")
+
+
 
 galleass.approveFirst("Dogger",2,"Harbor")
 
