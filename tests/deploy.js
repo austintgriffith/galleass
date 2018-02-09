c3cbdcb9a7c9928fa98e0fc2ec6510e680d3c21e
Austin Griffith <austin@pyth.io>
2018-02-09 03:12:19 +0000

    uncomment

--- tests/deploy.js
+++ tests/deploy.js
@@ -1,64 +1,64 @@
 const galleass = require("./galleass.js")
 
-//
-// galleass.deploy("Galleass",0)
+
+galleass.deploy("Galleass",0)
 
 galleass.deploy("Experience",0)
 galleass.setContract("Experience",0)
-//
-// galleass.deploy("Sea",0)
-// galleass.setContract("Sea",0)
-// galleass.setPermission("Sea",0,"transferDogger","true")
-// galleass.setPermission("Sea",0,"updateExperience","true")
-//
-// galleass.deploy("Land",0)
-// galleass.setContract("Land",0)
-//
-// galleass.deploy("Dogger",0)
-// galleass.setContract("Dogger",0)
-//
-// galleass.deploy("Timber",0)
-// galleass.setContract("Timber",0)
-//
-// galleass.deploy("Copper",0)
-// galleass.setContract("Copper",0)
-//
-// galleass.deploy("Catfish",0)
-// galleass.setContract("Catfish",0)
-//
-// galleass.deploy("Pinner",0)
-// galleass.setContract("Pinner",0)
-//
-// galleass.deploy("Redbass",0)
-// galleass.setContract("Redbass",0)
-//
-// galleass.deploy("Snark",0)
-// galleass.setContract("Snark",0)
-//
-// galleass.deploy("Dangler",0)
-// galleass.setContract("Dangler",0)
-//
-// galleass.deploy("Fillet",0)
-// galleass.setContract("Fillet",0)
-//
-//
-// galleass.deploy("Harbor",0)
-// galleass.setContract("Harbor",0)
-// galleass.setPermission("Harbor",0,"buildDogger","true")
-// galleass.setPermission("Harbor",0,"updateExperience","true")
-//
-// galleass.deploy("Fishmonger",0)
-// galleass.setContract("Fishmonger",0)
-// galleass.setPermission("Fishmonger",0,"transferFish","true")
-// galleass.setPermission("Fishmonger",0,"mintFillet","true")
-// galleass.setPermission("Fishmonger",0,"updateExperience","true")
-//
-// galleass.testMint("Copper",0,1,100)
-// galleass.approveContract("Copper",1,"Fishmonger",100)
-// galleass.transferTokens("Copper",1,"Fishmonger",100)
-//
-// galleass.setFishPrice(0,"Pinner",1)
-// galleass.setFishPrice(0,"Redbass",2)
-// galleass.setFishPrice(0,"Catfish",3)
-// galleass.setFishPrice(0,"Snark",5)
-// galleass.setFishPrice(0,"Dangler",7)
+
+galleass.deploy("Sea",0)
+galleass.setContract("Sea",0)
+galleass.setPermission("Sea",0,"transferDogger","true")
+galleass.setPermission("Sea",0,"updateExperience","true")
+
+galleass.deploy("Land",0)
+galleass.setContract("Land",0)
+
+galleass.deploy("Dogger",0)
+galleass.setContract("Dogger",0)
+
+galleass.deploy("Timber",0)
+galleass.setContract("Timber",0)
+
+galleass.deploy("Copper",0)
+galleass.setContract("Copper",0)
+
+galleass.deploy("Catfish",0)
+galleass.setContract("Catfish",0)
+
+galleass.deploy("Pinner",0)
+galleass.setContract("Pinner",0)
+
+galleass.deploy("Redbass",0)
+galleass.setContract("Redbass",0)
+
+galleass.deploy("Snark",0)
+galleass.setContract("Snark",0)
+
+galleass.deploy("Dangler",0)
+galleass.setContract("Dangler",0)
+
+galleass.deploy("Fillet",0)
+galleass.setContract("Fillet",0)
+
+
+galleass.deploy("Harbor",0)
+galleass.setContract("Harbor",0)
+galleass.setPermission("Harbor",0,"buildDogger","true")
+galleass.setPermission("Harbor",0,"updateExperience","true")
+
+galleass.deploy("Fishmonger",0)
+galleass.setContract("Fishmonger",0)
+galleass.setPermission("Fishmonger",0,"transferFish","true")
+galleass.setPermission("Fishmonger",0,"mintFillet","true")
+galleass.setPermission("Fishmonger",0,"updateExperience","true")
+
+galleass.testMint("Copper",0,1,100)
+galleass.approveContract("Copper",1,"Fishmonger",100)
+galleass.transferTokens("Copper",1,"Fishmonger",100)
+
+galleass.setFishPrice(0,"Pinner",1)
+galleass.setFishPrice(0,"Redbass",2)
+galleass.setFishPrice(0,"Catfish",3)
+galleass.setFishPrice(0,"Snark",5)
+galleass.setFishPrice(0,"Dangler",7)
