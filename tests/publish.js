990af493ecdf21a889daf78df65b5618e207b62c
Austin Griffith <austin@pyth.io>
2018-02-09 03:09:06 +0000

    deploy Experience contract and start working on contracts page

--- tests/publish.js
+++ tests/publish.js
@@ -1,2 +1,8 @@
 const galleass = require("./galleass.js")
+
 galleass.publish()
+
+const fs = require("fs");
+let content = fs.readFileSync("app/public/contracts.template").toString();
+
+console.log(content);
