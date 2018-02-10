a4284a98f66fab62d10d5fc80d86692bbc5728c1
Austin <austin.griffith@madwiremedia.com>
2018-02-10 06:44:58 -0700

    publish main contract

--- tests/publish.js
+++ tests/publish.js
@@ -1,6 +1,6 @@
-// const galleass = require("./galleass.js")
-//
-// galleass.publish()
+const galleass = require("./galleass.js")
+
+galleass.publish()
 
 const fs = require("fs");
 let content = fs.readFileSync("app/public/contracts.template").toString();
